8fe4182bfacda2526a2b976bd88affcd45d93207
erin mcdonagh <erinemcdonagh@hotmail.com>
2017-10-27 15:23:17 -0700

    working on issues... style.css: creates translucent nav, changes font... function.php: adds custom font w/changes, fix issues...adds post clone to edit continue reading section...

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -1,4 +1,8 @@
-<link href="https://fonts.googleapis.com/css?family=Walter+Turncoat" rel="stylesheet">
+
+
+
+
+
 
 <?php
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
@@ -9,3 +13,15 @@ function theme_enqueue_styles() {
        array('parent-style')
    );
 }
+
+/* adds custom fonts */
+
+function twentyseventeen_child_scripts() {
+
+    wp_enqueue_style('twentyseventeen-child-fonts', 'https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300,400i,500,700,700i,800,900|Alegreya:400,400i,700,700i" rel="stylesheet');
+}
+
+    add_action('wp-enqueue-scripts', 'twentyseventeen_child_scripts');
+    
+    
+  
