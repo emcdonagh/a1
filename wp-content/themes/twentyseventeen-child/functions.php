c886a6644f86117fc83336e6d72c05617a78657b
erin mcdonagh <erinemcdonagh@hotmail.com>
2017-10-27 16:13:25 -0700

    style.css: chanfes to links, takes away underline and colours.

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -18,7 +18,7 @@ function theme_enqueue_styles() {
 
 function twentyseventeen_child_scripts() {
 
-    wp_enqueue_style('twentyseventeen-child-fonts', 'https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300,400i,500,700,700i,800,900|Alegreya:400,400i,700,700i" rel="stylesheet');
+    wp_enqueue_style('twentyseventeen-child-fonts', 'https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:100,300,400,400i,500,700,700i,800,900|Life+Savers:400,700" rel="stylesheet');
 }
 
     add_action('wp-enqueue-scripts', 'twentyseventeen_child_scripts');
